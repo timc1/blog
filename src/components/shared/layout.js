bca279050ceafff6a4b907bfa1be472f5f6d1512
Tim <timchang.tcc@gmail.com>
2019-01-05 22:13:37 -0800

    Add prism theme

--- src/components/shared/layout.js
+++ src/components/shared/layout.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import './layout.css'
+import './prism-github-theme.css'
 
 export default ({ children }) => (
   <>
